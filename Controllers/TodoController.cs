4b412368e0707a5ea44cdcdb10c92e2b7a7bd0cb
mamanasaf1@gmail.com <mamanasaf1@gmail.com>
2017-11-09 09:16:43 +0200

    Dlls

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -11,20 +11,27 @@ namespace TodoApi.Controllers
 
         }
         [HttpGet]
-        public IEnumerable<TodoItem> GetAll(){
-            var items=new List<TodoItem>{
+        public IEnumerable<TodoItem> GetAll()
+        {
+            List<TodoItem> items = NewMethod();
+
+            return items;
+        }
+
+        [HttpGet("{id}",Name="GetTodo")]
+        public IActionResult GetItemById(long id){
+            var item=new TodoItem{Id=id,Name="item "+id,IsComplete=true};
+            return new OkObjectResult(item);
+        }
+
+        private static List<TodoItem> NewMethod()
+        {
+            return new List<TodoItem>{
                 new TodoItem{Id=1,Name="Item 1",IsComplete=false},
                 new TodoItem{Id=2,Name="Item 2",IsComplete=false},
                 new TodoItem{Id=3,Name="Item 3",IsComplete=true},
                 new TodoItem{Id=4,Name="Item 4",IsComplete=true},
             };
-            
-            return items;
-        } 
-        [HttpGet("{id}",Name="GetTodo")]
-        public IActionResult GetItemById(long id){
-            var item=new TodoItem{Id=id,Name="item "+id,IsComplete=true};
-            return new OkObjectResult(item);
         }
     }
 }
